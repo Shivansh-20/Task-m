91359ce8637d4a43ba18d6829183b448f7e59b1a
Shivansh Tyagi <135636253+Shivansh-20@users.noreply.github.com>
2026-08-27 21:14:25 +0530

    Auto-sync: file changes

--- apip.py
+++ apip.py
@@ -1,6 +1,7 @@
 import sqlite3
 connection = sqlite3.connect("trading.db")
 cursor = connection.cursor()
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS portfolio(
 ticker TEXT,
@@ -11,3 +12,8 @@ average_buyprice REAL  #like float
 )
 connection.commit() #can use with for automatic but not good practise
 connection.close()
+prices = {
+    "TCS":3900,
+    "INFO":3000,
+    "HCL":2800
+}
