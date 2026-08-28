e774d6bd2c7a572ceffc0ebfbad576fe31916a0e
Shivansh Tyagi <135636253+Shivansh-20@users.noreply.github.com>
2026-08-28 19:07:12 +0530

    Auto-sync: file changes

--- apip.py
+++ apip.py
@@ -1,5 +1,6 @@
 import sqlite3
 from pydantic import BaseModel
+from fastapi import FastAPI
 connection = sqlite3.connect("trading.db")
 cursor = connection.cursor()
 
@@ -21,3 +22,21 @@ prices = {
 class Trade(BaseModel):
     ticker: str
     quantity: int
+
+
+app = FastAPI()
+
+@app.post("/trade")
+def trade_stock(trade:Trade):
+    ticker = trade.ticker
+    quantity = trade.quantity
+    price = prices[ticker] #store the value in price from dict price of company in ticker
+
+
+cursor.execute(
+    "Select qauntity from portfolio where ticker = ?",
+    (ticker,)
+)
+
+
+
