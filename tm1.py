6bbe4723e193b6588e1af5126c3c55c56dadcc55
Shivansh Tyagi <135636253+Shivansh-20@users.noreply.github.com>
2026-09-04 22:45:58 +0530

    Auto-sync: file changes

--- tm1.py
+++ tm1.py
@@ -1,5 +1,6 @@
 import sys
 import json
+#welcome Feature
 from datetime import datetime
 if sys.argv[1].strip().lower() == "welcome":
     print("""Weclome, command words for input
@@ -7,14 +8,16 @@ if sys.argv[1].strip().lower() == "welcome":
 -- for checking tasks -> "list"  as action - > payload accepted ->  "done" and -> "not done"
 -- for  printing all tasks -> use only "list"
 -- To delete a task - >"delete" "task ID as payload" """)
+#initializing action and payload
 action = sys.argv[1].strip().lower()
 #payload = sys.argv[2]
 print(f"Action -> {action}")
 payload = " ".join(sys.argv[2:]).strip().lower()
 print("Payload is", payload or None)
-
+#initalizing "DATA"
 with open("data.txt", "r") as file:
     data = json.load(file)
+#initial feature deisgn, ignore
 '''
 def read_name(data):
     print(data["name"])
@@ -32,7 +35,7 @@ def change_name(data,new):
         json.dump(data,file)
 
 '''
-
+#actual function design
 def add_task(data,payload):
     if data:
         max_id = max(item['id']for item in data)
@@ -47,7 +50,7 @@ def add_task(data,payload):
     data.append(new_task)
     with open("data.txt" , "w") as file:
         json.dump(data, file, indent = 4)
-
+#delete func
 def delete_tasks(data,payload):
     task_id = int(payload)
     for i, item in enumerate(data):
@@ -55,29 +58,29 @@ def delete_tasks(data,payload):
             data.pop(i)
             print("deleted successfully")
             break
-        else:
+    else:
             print("Task not found")
-
+#commiting back to file
     with open("data.txt", "w") as file:
         json.dump(data,file,indent=4)
-
-
-
+#calling the functions
 if action == "add":
     add_task(data,payload)
     print("task added successfully")
 elif action == "list":
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 2: #to make sure it does not give out of bound error while checking
         if payload == "done":
             print([items for items in data if items.get("status")== "done"] or None)
         elif payload == "not done":
             print([items for items in data if items.get("status")== "not done"] or None)
     elif not payload:
+        #new design
         if data:
             for item in data:
                 print(item)
         else:
             print("nothing to show")
+            #initial design
         '''for item in data:
             print(item or "nothing to show")
             For every item in data, print the item; if that particular item is empty, print "nothing to show"'''
